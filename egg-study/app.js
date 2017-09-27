26380afc8dcf485226fa959d08ff98340979d2ce
stanleychen96 <stanleychen@yeah.net>
2017-09-27 16:15:26 +0800

    add egg-logger

--- egg-study/app.js
+++ egg-study/app.js
@@ -14,6 +14,7 @@ const knex = require('knex')({
 module.exports = app => {
   app.beforeStart(function* () {
     const ctx = app.createAnonymousContext();
+    const start = new Date();
     const havesisters = yield app.mysql.query(knex.schema.hasTable('member').toString());
     if (havesisters.length === 0) {
       const userid = knex.schema.createTableIfNotExists('member', function(level) {
@@ -34,9 +35,12 @@ module.exports = app => {
       yield app.mysql.query(uniqueName.toString());
       
       ---------------- have the same function beside -----------------*/
-
       yield ctx.helper.men(app, 'member', 'name');
+
     }
+    ctx.logger.warn('WARNNING!!!!');    
+    ctx.logger.info('some request data: %j', ctx.request.body);
+    ctx.logger.info(1000, Date.now() - start);
   });
 
 // ------both use 'knex.schema.alterTable',the second more simple-----
